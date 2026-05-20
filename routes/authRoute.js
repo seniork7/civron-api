2b5a57dc89699b361e2f3ea7050d8693e4922a59
Kevon Senior <98934481+seniork7@users.noreply.github.com>
2026-05-19 23:05:30 -0400

    Create a /check-auth route to handle staff info rehydration on the front-end

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,9 +4,23 @@
 
 import express from 'express';
 import loginStaff from '../controllers/loginStaffController.js';
+import verifyStaff from '../middleware/verifyStaff.js';
 
 const router = express.Router();
 
 router.post('/login', loginStaff);
+router.get('/check-auth', verifyStaff, async (req, res) => {
+	const staff = req.staff;
+	res.json({
+		message: 'Authenticated',
+		role: staff.role,
+		fName: staff.fName,
+		lName: staff.lName,
+		title: staff.title,
+		orgID: staff.orgID,
+		orgName: staff.orgName,
+		country: staff.country,
+	});
+});
 
 export default router;
